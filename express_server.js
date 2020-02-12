d60bddfd100bac9e97fca5c1a72397b46dae158d
Raphael Albern <ralpha19@hotmail.com>
2020-02-12 00:56:06 +0000

    added registration and login page

--- express_server.js
+++ express_server.js
@@ -25,27 +25,82 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// USERS DATABASE
+const users = {
+  "userRandomID": {
+                    id: "userRandomID",
+                    email: "user@example.com",
+                    password: "purple-monkey-dinosaur"
+                  },
+ "user2RandomID": {
+                    id: "user2RandomID",
+                    email: "user2@example.com",
+                    password: "dishwasher-funk"
+                  }
+}
+
+// EMAIL LOOKUP FUNCTION
+function emailLookUp(input) {
+  for (let user in users) {
+    if (input === users[user].email) {
+      return true;
+    }
+  }
+}
+
+//PASSWORD LOOKUP FUNCTION
+function passwordLookUp(input) {
+  for (let user in users) {
+    if (input === users[user].password) {
+      return true;
+    }
+  }
+}
+
+
 // DISPLAY SHORT AND LONG URLs
 app.get("/urls", (req, res) => {
   let templateVars = {
-                        username: req.cookies["username"],
+                        users: users,
+                        user_id: req.cookies["user_id"],
                         urls: urlDatabase
                       };
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
-// RENDER PAGE TO CREATE NEW URL
+// PASSING DATA TO URLS_NEW & RENDER PAGE TO CREATE NEW URL
 app.get('/urls/new', (req, res) => {
   let templateVars = {
-                      username: req.cookies["username"],
+                      users: users,
+                      user_id: req.cookies["user_id"],
                       urls: urlDatabase
                     };
   res.render('urls_new', templateVars);
 });
+
+// PASSING DATA TO /REGISTER & RENDER /REGISTER PAGE
+app.get('/register', (req, res) => {
+  let templateVars = {
+                      users: users,
+                      user_id: req.cookies["user_id"],
+                      urls: urlDatabase
+                    };
+  res.render('register', templateVars)
+})
+
+// PASSING DATA TO /LOGIN & RENDER /LOGIN PAGE
+app.get('/login', (req, res) => {
+  let templateVars = {
+                      users: users,
+                      user_id: req.cookies["user_id"],
+                      urls: urlDatabase
+                    };
+  res.render('login', templateVars);
+})
 
 // ADD NEW SHORT & LONG URL TO DATABASE
 app.post("/urls", (req, res) => {
@@ -57,7 +112,8 @@ app.post("/urls", (req, res) => {
 // DISPLAY LONG & SHORT URL FOR SPECIFIC SHORT URL
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
-                       username: req.cookies["username"],
+                       users: users,
+                       user_id: req.cookies["user_id"],
                        urls: urlDatabase,
                        shortURL: req.params.shortURL,
                        longURL: urlDatabase[req.params.shortURL]
@@ -71,24 +127,55 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+// DELETE URL
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls')
 });
 
+// EDIT URL
 app.post("/urls/:id/update", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect('/urls')
 })
 
+// EDGE CASE FOR REGISTERING NEW USER + SET COOKIE
+app.post("/register", (req, res) => {
+  let randomUser = `user${generateRandomString()}`;
+  // send error message if user leave email and/or password fields blank
+  if (req.body.email === "" || req.body.password === "") {
+    res.send('Error - 400 Bad Request. Email or Password cannot be blank.')
+  }
+  // send error message if user register with an existing email,
+  else if (emailLookUp(req.body.email) === true) {
+    res.send('Error - 400 Bad Request. Email already exist.')
+  }
+  users[randomUser] = {
+                        id: randomUser,
+                        email: "user1@example.com",
+                        password: "test"
+                     }
+  res.cookie('user_id', 'user_id');
+  res.redirect('/urls');
+});
+
+// EDGE CASE WHEN EXISTING USER LOGIN + SET COOKIE
 app.post("/login", (req, res) => {
-  res.cookie('username', 'username');
-  console.log("cookie: ", req.cookies['username'])
+  // send error message if user login with incorrect email
+  if (!emailLookUp(req.body.email)) {
+    res.send('Error - 403 Forbidden. Email does not match.')
+  }
+  // sent error message if user input incorrect password
+  else if (!passwordLookUp(req.body.password)) {
+    res.send('Error - 403 Forbidden. Password does not match.')
+  }
+  res.cookie('user_id', 'user_id');
   res.redirect("/urls");
 });
 
+// CLEAR COOKIE WHEN USER LOGOUT
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('/urls');
 })
 
